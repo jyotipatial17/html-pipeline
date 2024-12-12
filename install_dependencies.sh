b2d9de015bf5b7879076dd62a82baed6723c5875
Jyoti Patial <jasminethakur.joy1@gmail.com>
2024-12-12 11:43:56 +0530

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+
+echo "Deploying files..."
+cp -r /tmp/my_artifact/* /var/www/html/
 yum update -y
 yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
+systemctl restart httpd 
+
